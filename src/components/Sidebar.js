f05d49d072475c0e2c3d9f3e2f5f92059eb47029
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-08-16 15:21:59 +0200

    first push

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/sidebar.scss"
+
+function Sidebar() {
+  return (
+    <div className='cardsSidebar'>
+
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/components/Story.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/story.scss"
+
+function Story() {
+  return (
+    <div className='story'>
+
+    </div>
+  )
+}
+
+export default Story
